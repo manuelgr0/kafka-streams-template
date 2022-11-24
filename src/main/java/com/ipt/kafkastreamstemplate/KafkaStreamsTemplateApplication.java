6693958404028b73d0f4e2fc940ea84c18f11b1d
manuelgr <manuelgr77@outlook.com>
2022-11-24 15:12:39 +0100

    initial project setup

---
+++ src/main/java/com/ipt/kafkastreamstemplate/KafkaStreamsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ipt.kafkastreamstemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipt/kafkastreamstemplate/KafkaStreamsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipt.kafkastreamstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
